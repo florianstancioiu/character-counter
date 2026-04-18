ba0fb02f129a3a7e3c6684c02fed3b32382d34c4
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-04-18 13:28:51 +0300

    Used regex for stats
    Used regex space identifiers to replace and split the text that
    calculates the word count and sentence count

--- src/utilities/stats.ts
+++ src/utilities/stats.ts
@@ -1,12 +1,12 @@
 export default (text: string, excludeSpaces: boolean = false) => {
   const trimmedText = text.trim();
   const totalCharacters = excludeSpaces
-    ? trimmedText.replace(" ", "").length
+    ? trimmedText.replace(/\s+/, "").length
     : trimmedText.length;
   const wordCount =
-    trimmedText.length === 0 ? 0 : trimmedText.split(" ").length;
+    trimmedText.length === 0 ? 0 : trimmedText.split(/\s+/).length;
   const sentenceCount =
-    trimmedText.length === 0 ? 0 : trimmedText.split(".").length;
+    trimmedText.length === 0 ? 0 : trimmedText.split(/\.+/).length;
 
   return {
     totalCharacters,
