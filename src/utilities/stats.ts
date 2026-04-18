93950aaa156ff5a87e5a45bbf99ce203a087ddd7
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-04-18 14:08:07 +0300

    Computed stats using global regex flags

--- src/utilities/stats.ts
+++ src/utilities/stats.ts
@@ -1,12 +1,12 @@
 export default (text: string, excludeSpaces: boolean = false) => {
   const trimmedText = text.trim();
   const totalCharacters = excludeSpaces
-    ? trimmedText.replace(/\s+/, "").length
+    ? trimmedText.replace(/\s+/gm, "").length
     : trimmedText.length;
   const wordCount =
-    trimmedText.length === 0 ? 0 : trimmedText.split(/\s+/).length;
+    trimmedText.length === 0 ? 0 : trimmedText.split(/\s+/gm).length;
   const sentenceCount =
-    trimmedText.length === 0 ? 0 : trimmedText.split(/\.+/).length;
+    trimmedText.length === 0 ? 0 : trimmedText.split(/\.+/gm).length;
 
   return {
     totalCharacters,
